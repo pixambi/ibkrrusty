36b394e70f5cbf78c5f4fed1bb41d4984e08561b
pixam <github@pixelizedambitions.com>
2025-08-01 23:50:52 +1000

    Add initial HTTP client implementation
    Introduced new modules: client, endpoints, and models for HTTP interactions. Updated Cargo.toml to include dependencies (reqwest, serde_json, url) and exposed new functionality in lib.rs. This sets up the foundation for making HTTP requests and handling API endpoints.

---
+++ src/client.rs
@@ -0,0 +1,66 @@
+use reqwest::header::{AUTHORIZATION, HeaderMap, HeaderValue};
+use reqwest::{Client as HttpClient, Method, RequestBuilder};
+use url::{ParseError, Url};
+
+#[derive(Clone, Debug)]
+pub struct Client {
+    pub(crate) http: HttpClient,
+    pub(crate) base_url: url::Url,
+}
+
+#[derive(Debug)]
+pub enum ClientError {
+    ParseError(ParseError),
+    RequestError(reqwest::Error),
+}
+impl From<ParseError> for ClientError {
+    fn from(err: ParseError) -> Self {
+        ClientError::ParseError(err)
+    }
+}
+
+impl From<reqwest::Error> for ClientError {
+    fn from(err: reqwest::Error) -> Self {
+        ClientError::RequestError(err)
+    }
+}
+
+impl Client {
+    pub fn new(token: &str) -> Result<Self, ClientError> {
+        Ok(Self {
+            http: HttpClient::new(),
+            base_url: url::Url::parse("https://localhost:5000/v1/api/")?,
+        })
+    }
+
+    //Request builder with authentication
+    pub(crate) fn request(
+        &self,
+        method: Method,
+        url: url::Url,
+    ) -> Result<RequestBuilder, ClientError> {
+        let headers = self.auth_headers()?;
+        Ok(self.http.request(method, url).headers(headers))
+    }
+
+    /// Helper method to handle responses that should return 204 No Content
+    pub(crate) async fn handle_no_content_response(
+        &self,
+        response: reqwest::Response,
+    ) -> Result<(), ClientError> {
+        match response.status() {
+            reqwest::StatusCode::NO_CONTENT => Ok(()),
+            _ => {
+                response
+                    .error_for_status()
+                    .map(|_| ()) // Convert success response to ()
+                    .map_err(ClientError::RequestError)
+            }
+        }
+    }
+
+    fn auth_headers(&self) -> Result<HeaderMap, ClientError> {
+        let headers = HeaderMap::new();
+        Ok(headers)
+    }
+}
